934713756d5ba216a1b609823e254f221378e11d
Jilin Men <1032764332@qq.com>
2025-03-22 15:58:01 -0500

    Update 0_🌊_QuickView_WaterQuality.py

--- pages/0_🌊_QuickView_WaterQuality.py
+++ pages/0_🌊_QuickView_WaterQuality.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore")
 
 @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
-    geemap.ee_initialize(token_name="EARTHENGINE_TOKEN")
+    geemap.ee_initialize(token_name=token_name)
 
 
 st.sidebar.info(
