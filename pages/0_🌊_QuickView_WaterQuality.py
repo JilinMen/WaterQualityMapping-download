8391558308a883cdac3e69c4ae157069e09468f3
Jilin Men <1032764332@qq.com>
2025-03-24 13:12:27 -0500

    Update 0_🌊_QuickView_WaterQuality.py

--- pages/0_🌊_QuickView_WaterQuality.py
+++ pages/0_🌊_QuickView_WaterQuality.py
@@ -59,11 +59,11 @@ with col1:
 
     import json
     # 获取Streamlit组件
-    st_component = m.st_folium  # 获取对应的 streamlit-folium 组件
-    
+    draw_features = m.st_draw_features  # 获取对应的 streamlit-folium 组件
+    st.write("Draw features:", draw_features)
     # 调用 st_last_draw() 获取最后一次绘制的结果
-    draw_data = m.st_last_draw(st_component)  # 调用方法，传入 st_component
-    
+    last_draw = m.st_last_draw(st_component)  # 调用方法，传入 st_component
+    st.write("Draw last:", last_draw)
     # 尝试解析结果，假设返回的是有效的GeoJSON字符串
     try:
         draw_data_json = json.loads(draw_data)  # 将字符串解析为字典
